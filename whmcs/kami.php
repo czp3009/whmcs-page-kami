08acd1748b6ef5e5d7f7a3f7285a8c92a01c4fac
czp3009 <czp3009@gmail.com>
2016-02-16 15:19:56 +0800

    Create kami.php

---
+++ whmcs/kami.php
@@ -0,0 +1,40 @@
+<?php
+
+use WHMCS\Database\Capsule;
+
+define("CLIENTAREA",true);
+//define("FORCESSL",true); // Uncomment to force the page to use https://
+
+require("init.php");
+
+$ca = new WHMCS_ClientArea();
+
+$ca->setPageTitle("卡密充值系统");
+
+$ca->addToBreadCrumb('index.php', Lang::trans('globalsystemname'));
+$ca->addToBreadCrumb('mypage.php','卡密充值');
+
+$ca->initPage();
+
+//$ca->requireLogin(); // Uncomment this line to require a login to access this page
+
+# To assign variables to the template system use the following syntax.
+# These can then be referenced using {$variablename} in the template.
+
+# Check login status
+if ($ca->isLoggedIn()) {
+    
+    # User is logged in - put any code you like here
+    $ca->setTemplate('kami');
+
+} else {
+    
+    # User is not logged in
+    $ca->setTemplate('kami_withoutlogin');
+
+}
+    $ca->output();
+    
+# Define the template filename to be used without the .tpl extension
+
+ ?>
