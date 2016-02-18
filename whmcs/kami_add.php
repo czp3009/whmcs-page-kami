5982f541c36ee988c0ae83fefc6c560fb409130c
czp3009 <czp3009@gmail.com>
2016-02-18 14:51:03 +0800

    Update and rename kami_get.php to kami_add.php

--- whmcs/kami_add.php
+++ whmcs/kami_add.php
@@ -1,4 +1,4 @@
-<form id="form1" name="form1" method="post" action="kami_get.php?action=sc">
+<form id="form1" name="form1" method="post" action="kami_add.php?action=sc">
   <p>生成数量：
     <label for="sl"></label>
     <input type="text" name="sl" id="sl" />
@@ -20,26 +20,28 @@
   </p>
 </form>
 <?php
+//mysql config
+$address="100.98.146.94";  //mysql host
+$database="whmcs";  //mysql database
+
 $username=$_POST["username"];
 $password=$_POST["password"];
-mysql_connect("100.98.146.94",$username,$password);
-mysql_select_db("whmcs");
+mysql_connect($address,$username,$password);
+mysql_select_db($database);
 if ($_GET["action"]=="sc"){
 echo "Get Card Successful.<br>";
 $sl=$_POST["sl"];
 $value=$_POST["value"];
 for ($i=1;$i<=$sl;$i++){
 	for ($j=1;$j<=32;$j++){
-		$kami1=$kami1.chr(rand(65,90));
+		$kami=$kami.chr(rand(65,90));
 	}
-	$kami=rand(10000,99999)."_".$kami1;
 	echo $kami;
 	$control=mysql_query("INSERT INTO kami (card,credit) VALUE ('$kami',$value)");
-    if($control==false) echo " FAIL";
+    if(!$control) echo " FAIL";
     else echo " SUCCESS";
     echo "</br>";
 	$kami="";
-	$kami1="";
 	}
 }
 ?>
